e475371fa9859b22add57ce7e32aa71cf19ed41a
Karkar1ch <karina.rogalevich@email.com>
2024-08-08 11:39:43 +0200

    Пофиксила app начала верстать

--- client/my-app/src/components/LoginForm.tsx
+++ client/my-app/src/components/LoginForm.tsx
@@ -7,19 +7,33 @@ const LoginForm: FC = () => {
     const {store} = useContext(Context);
 
     return (
-        <div>
+        <div className = 'window'>
+            <div className="window_headr">
+                <div className="window_headr_logo"><img src={`${process.env.PUBLIC_URL}/messenger (3).png`} alt="Messenger" /></div>
+                <div className="window_headr_h">
+                    <h1>Messanger</h1>
+                    <p>Войдите в аккаунт или зарегистрируйтесь</p>
+                </div>
+            </div>
+            <div className="window_input">
             <input 
                 onChange={e => setEmail(e.target.value)}
                 value={email}
                 type='email' 
-                placeholder='Email'/>
+                placeholder='Email'
+                className = 'Email'/>
             <input 
                 onChange={e => setPassword(e.target.value)}
                 value={password}
                 type='password' 
-                placeholder='Пароль'/>
-            <button onClick={() => store.login(email, password)}>Логин</button>
-            <button onClick={() => store.registration(email, password)}>Регистрация</button>
+                placeholder='Пароль'
+                className = 'Password'/>
+            </div>
+            <a href='#' className = 'a'>Забыли пароль?</a>
+            <div className = "button">
+                <button onClick={() => store.login(email, password)}>Логин</button>
+                <button onClick={() => store.registration(email, password)}>Регистрация</button>
+            </div>
         </div>
     )
 }
